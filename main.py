347f7e3fd3dee3862111b6facb0fa1c258af5a5c
Artis7eer <danabraham777@gmail.com>
2021-07-14 15:31:06 +0530

    Add Licence Header

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+# URLShortenerBot - A Telegram Bot To short long urls
+#Copyright (C) 2021 by Rasak <https://github.com/Artis7eeR>
+#URLShortenerBot is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#URLShortenerBot is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from telegram.ext import Filters,MessageHandler,CommandHandler,Updater,CallbackQueryHandler
 from config import Config
 from plugins.commands import (
